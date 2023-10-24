0fd9fe168c7e68829585fcd44ede5f42749479ef
johnfaber2801 <johnfaber2801@hotmail.com>
2023-10-24 18:47:18 +1100

    Error handlers, added funtions for all main.py

--- src/main.py
+++ src/main.py
@@ -2,15 +2,16 @@ from analyzer import Analisis_text
 from analyzer import Word_Frequency
 from analyzer import Keyword
 
-file_path= 'src/enter_your_text_here.txt'
-with open(file_path) as file:
-    file_text = file.read()
 
-analyzer = Analisis_text(file_text)
-frequency = Word_Frequency(file_text)
-keywords = Keyword(file_text)
+def read_file(file_path):
+     try:        
+        with open(file_path) as file:
+         return file.read()
+    
+     except Exception as e:
+        print(f"Error: {e}")
 
-        # Choice #1
+        # Feature #1
 def analyze_text(analyzer):
     analyzer.word_count()
     analyzer.character_count()
@@ -18,47 +19,66 @@ def analyze_text(analyzer):
     analyzer.paragraph_count()
     analyzer.language_text()
 
-print('--------------------------------')
-print('Welcome to Text File Analyzer')
-print('--------------------------------')
+def main():
+                #file path must be specified here
+    file_path = 'src/enter_your_text_here.txt'
+    file_text = read_file(file_path)
 
-while True:    
-    
-        print('Choose an option')
-        print('1. Text Analysis')
-        print('2. Word Frequency Analysis')
-        print('3. Keywords extraction')
-        print('4. Exit')
+    if file_text:
 
-        choice = input('Enter choice:  ')
+        analyzer = Analisis_text(file_text)
+        frequency = Word_Frequency(file_text)
+        keywords = Keyword(file_text)
+                
+        print('--------------------------------')
+        print('Welcome to Text File Analyzer')
+        print('--------------------------------')
 
-        if choice == '1': 
-            analyze_text(analyzer)
-
-        elif choice == '2':
-            word_frequency = frequency.word_frequency()
-
-        elif choice == '3':
-            keywords_extracted = keywords.keyword_extraction()
+    while True:    
         
-        elif choice == '4':
-            print('Thanks for using Text File Analizer! ')
-            break
+            print('Choose an option')
+            print('1. Text Analysis')
+            print('2. Word Frequency Analysis')
+            print('3. Keywords extraction')
+            print('4. Exit')
 
-        else: 
-            print('--------------------------------')
-            print('Invalid choice, Please try again')
-            print('--------------------------------')
+            choice = input('Enter choice:  ')
 
-        while True:
-            continue_option = input('Would you like to continue? (yes/no): ').lower()
-            if continue_option in ['yes', 'no']:
-                break
-            else:
-                print('Invalid input. Please enter "yes" or "no".')
+            try:
+                if choice == '1': 
+                    analyze_text(analyzer)
 
-        if continue_option != 'yes':
-            print('--------------------------------')
-            print('Thanks for using Text File Analizer! ')
-            print('--------------------------------')
-            break
+                elif choice == '2':
+                    frequency.word_frequency()
+
+                elif choice == '3':
+                    keywords.keyword_extraction()
+                
+                elif choice == '4':
+                    print('--------------------------------')
+                    print('Thanks for using Text File Analizer! ')
+                    print('--------------------------------')
+                    break
+                else: 
+                    print('--------------------------------')
+                    print('Invalid choice, Input must be number 1, 2, 3 or 4.\nPlease try again!')
+                    print('--------------------------------')
+               
+                while True:
+                     continue_option = input('Would you like to continue? (yes/no): ').lower()
+                     if continue_option in ['yes', 'no']:
+                        break                       
+                     else:
+                        print("Invalid choice, Input must be 'yes' or 'no'")    
+
+                if continue_option != 'yes':
+                    print('--------------------------------')
+                    print('Thanks for using Text File Analyzer!')
+                    print('--------------------------------')
+                    break     
+                       
+            except Exception as e:
+             print(f"Error: {e}")
+            
+if __name__ == "__main__":
+    main()
